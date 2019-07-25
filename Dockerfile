485fd87451e08c2ead2a02c750a8539175734b1e
Kaleb Fischer <kalebfischer@flywheel.io>
2019-07-25 12:57:02 -0500

    initial add file updates for optibet

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,19 @@
 # Create a base docker container that will run FSL's SIENA command
 
-FROM flywheel/fsl-base:5.0_neurodeb
+FROM flywheel/fsl-siena:1.0.0_5.0
 MAINTAINER Flywheel <support@flywheel.io>
 
-
-# Install python package dependencies
-COPY requirements.txt ./requirements.txt
-RUN pip3 install -r requirements.txt
+# Copy optiBET scripts
+COPY optiBET.sh /usr/local/bin/optiBET.sh
+RUN chmod +x /usr/local/bin/optiBET.sh
+COPY siena_optibet /usr/lib/fsl/5.0/siena_optibet
+RUN chmod +x /usr/lib/fsl/5.0/siena_optibet
+COPY sienax_optibet /usr/lib/fsl/5.0/sienax_optibet
+RUN chmod +x /usr/lib/fsl/5.0/siena_optibet
 
 # Make directory for flywheel spec (v0)
 ENV FLYWHEEL /flywheel/v0
 RUN mkdir -p ${FLYWHEEL}
 COPY run.py ${FLYWHEEL}/run.py
-RUN chmod +x ${FLYWHEEL}/run.py
 COPY manifest.json ${FLYWHEEL}/manifest.json
+
--- run.py
+++ run.py
@@ -317,16 +317,20 @@ if __name__ == '__main__':
         fw = gear_context.client
         # Get output filepath
         output_directory = gear_context.output_dir
-
         # Get config options
         config = gear_context.config
+        # Get optiBET boolean
+        optibet = config.get('OPTIBET')
         # Initialize command_list
         command_list = list()
         # Determine if SIENA or SIENAX
         if gear_context.get_input('NIFTI_1') and gear_context.get_input('NIFTI_2'):
             # Add siena command to command list
-
-            command_list.append('siena')
+            siena_command = 'siena'
+            # Use optibet script when specified
+            if optibet:
+                siena_command = siena_command + '_optibet'
+            command_list.append(siena_command)
             log.info('Getting FSL {} Configuration...'.format(command_list[0].upper()))
             # Get inputs from manifest
             nifti_1 = gear_context.get_input('NIFTI_1')
@@ -354,7 +358,11 @@ if __name__ == '__main__':
 
         elif gear_context.get_input('NIFTI'):
             # Add sienax command to command list
-            command_list.append('sienax')
+            siena_command = 'sienax'
+            # Use optibet script when specified
+            if optibet:
+                siena_command = siena_command + '_optibet'
+            command_list.append(siena_command)
             log.info('Getting FSL {} Configuration...'.format(command_list[0].upper()))
             # Get inputs from manifest
             nifti = gear_context.get_input('NIFTI')
@@ -394,7 +402,7 @@ if __name__ == '__main__':
             subject_code = get_subject_code(fw, gear_context.destination['id'])
             timestamp = int(time.time())
             # Specify files to retain outside of the analysis archive
-            promote = ['report.{}'.format(command_list[0]), 'report.viena', '.metadata.json']
+            promote = ['report.{}'.format(command_list[0].split('_')[0]), 'report.viena', '.metadata.json']
             # Fix report images
             for html_file in ['report.html', 'reportviena.html']:
                 html_report_path = os.path.join(output_directory, html_file)
@@ -414,7 +422,7 @@ if __name__ == '__main__':
             zip_name = '{}_{}_outputs_{}'.format(subject_code, command_list[0], timestamp)
             zip_most_outputs(output_directory, zip_name, promote)
             # Get metadata
-            report_file_list = ['report.{}'.format(command_list[0]), 'report.viena']
+            report_file_list = ['report.{}'.format(command_list[0].split('_')[0]), 'report.viena']
             for report in report_file_list:
                 report_path = os.path.join(output_directory, report)
                 # If the report file exists, parse it
